793bc4014f26972aaf54088a7c8959701aad4468
Yabacoder <yabacoder@gmail.com>
2020-10-25 02:25:17 +0100

    Here we go again.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+import routes, { renderRoutes } from './routes';
+
+const history = createBrowserHistory();
+
+function App() {
+  return (
+    <div className="App"> 
+      <Router history={history}>
+      {renderRoutes(routes)}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/views/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div className="text-center">
+            <h3>Hello world!...</h3>
+        </div>
+    )
+}
+
+export default index
